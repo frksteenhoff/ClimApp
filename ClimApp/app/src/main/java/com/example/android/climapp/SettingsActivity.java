8bcde4820314339fb512c39092d7facac0d73b40
Henriette Steenhoff <henriette.steenhoff@gmail.com>
2017-10-10 15:10:24 +0200

    Basics added to project
    Classes for the different screens, file structure, preliminary additions
    to sign in/sign up screen added etc. small steps.

---
+++ ClimApp/app/src/main/java/com/example/android/climapp/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.climapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by frksteenhoff on 10-10-2017.
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
---
+++ ClimApp/app/src/main/java/com/example/android/climapp/SignInActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.climapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by frksteenhoff on 10-10-2017.
+ */
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signinup);
+    }
+
+}
