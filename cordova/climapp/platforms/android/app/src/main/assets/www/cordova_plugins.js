21d9ea62674eaa2a763e21e21fa068616a625a82
Boris Kingma <boris.kingma@gmail.com>
2019-02-10 11:26:19 +0100

    Android build

--- cordova/climapp/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ cordova/climapp/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
     "id": "cordova-plugin-geolocation.geolocation",
     "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
     "pluginId": "cordova-plugin-geolocation",
@@ -15,14 +23,6 @@ module.exports = [
     "runs": true
   },
   {
-    "id": "cordova-plugin-device.device",
-    "file": "plugins/cordova-plugin-device/www/device.js",
-    "pluginId": "cordova-plugin-device",
-    "clobbers": [
-      "device"
-    ]
-  },
-  {
     "id": "cordova-plugin-splashscreen.SplashScreen",
     "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
     "pluginId": "cordova-plugin-splashscreen",
@@ -34,10 +34,10 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-geolocation": "4.0.1",
   "cordova-plugin-device": "2.0.2",
-  "cordova-plugin-splashscreen": "5.0.2"
+  "cordova-plugin-geolocation": "4.0.1",
+  "cordova-plugin-splashscreen": "5.0.2",
+  "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
--- cordova/climapp/platforms/android/app/src/main/assets/www/js/index.js
+++ cordova/climapp/platforms/android/app/src/main/assets/www/js/index.js
@@ -16,10 +16,17 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 var app = {
+	knowledgeBase: undefined,
+	pageMap: undefined,
+	currentPageID: undefined,
+	
+	
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+		//this.onDeviceReady(); //call this to run on browser, as browser does not fire the event by itself.
     },
 
     // deviceready Event Handler
@@ -32,15 +39,119 @@ var app = {
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+		this.initListeners();
+		this.loadSettings();
+		this.loadUI( "dashboard" );
+		
+		this.updateLocation();
+		
+		//
+		
+    },
+	initListeners: function(){
+		// navigation menu
+		$("#js-navbar-toggle").on('click', function () {
+		  	$("#js-menu").toggle('active');
+		});
+	},
+	loadSettings: function(){
+		this.pageMap = { "dashboard": "page-hello-world.html",
+						 "settings": "page-hello-world.html" };
+		
+		//if ( localStorage.getItem("knowledgebase") !== null) {
+		//	this.knowledgeBase = JSON.parse( localStorage.getItem("knowledgebase") );
+		//}
+		//else{
+			this.knowledgeBase = { "position": { "lat": 0, 
+												 "lng": 0, 
+												 "timestamp": "" },
+								   "weather": {	"station": "",
+												"lat": 0,
+												"lng": 0,
+											    "distance": -1,
+												"utc": "",
+												"wbgt": -99,
+												"windchill": -99 }
+											};
+		//}
+	},
+	saveSettings: function(){
+		console.log( "saving settings" );
+		let jsonData = JSON.stringify( this.knowledgeBase );
+		localStorage.setItem("knowledgebase", jsonData );
+	},
+	updateLocation: function(){
+		var self = this; //copy current scope into local scope for use in anonymous function 
+		let options = {timeout: 30000 };
+		navigator.geolocation.getCurrentPosition( 
+			function( position ){ //on success
+				self.knowledgeBase.position.lat = position.coords.latitude;
+				self.knowledgeBase.position.lng = position.coords.longitude;
+				self.knowledgeBase.position.timestamp = new Date( position.timestamp ).toJSON();
+				
+				self.updateWeather();
+				
+				self.saveSettings();
+				self.updateUI();
+			}, 
+			function( error ){ //on error
+				console.log( error );
+			},
+			options
+		);
+	},
+	updateWeather: function(){
+		var self = this;
+		if( 'position' in this.knowledgeBase ){
+			let url = "https://www.sensationmapps.com/WBGT/api/worldweather.php";
+			let data = { "action": "helios",
+						 "lat": this.knowledgeBase.position.lat,
+					 	 "lon": this.knowledgeBase.position.lng,
+					 	 "utc": new Date().toJSON() };
+			$.get( url, 
+				   data, 
+				   function( output ){//on success
+					   let weather = JSON.parse( output );
+					   self.knowledgeBase.weather.station = weather.station;
+					   self.knowledgeBase.weather.distance = weather.distance;
+					   self.knowledgeBase.weather.utc = weather.utc;
+					   
+					   self.knowledgeBase.weather.lat = weather.lat;
+					   self.knowledgeBase.weather.lng = weather.lon;
+					   self.knowledgeBase.weather.wbgt = weather.wbgt_max;
+					   self.knowledgeBase.weather.windchill = weather.windchill;
+					   
+	   				   self.saveSettings();
+	   				   self.updateUI();
+			});
+		}
+	},
+	loadUI: function( pageid ){
+		var self = this;
+		$.get( this.pageMap[ pageid ], function( content ){
+			console.log( "loaded: " + pageid);
+			self.currentPageID = pageid;
+			$("#content").html( content );
+			self.updateUI();
+			//$("#tipOfTheDay").html( "Tip of the Day");
+		})
+	},
+	updateUI: function(){
+		if( this.currentPageID == "dashboard" ){
+			console.log("current page 'dashboard'");
+			if( 'weather' in this.knowledgeBase && this.knowledgeBase.weather.station !== "" ){
+				let distance = parseFloat( this.knowledgeBase.weather.distance ).toFixed(0);
+				
+				$("#station").html( this.knowledgeBase.weather.station + " ("+ distance +" km)" );
+				$("#utc").html( this.knowledgeBase.weather.utc );
+				
+				$("#wbgt").html( this.knowledgeBase.weather.wbgt );
+				$("#windchill").html( this.knowledgeBase.weather.windchill );
+				
+			}
+		}
+	}
+	
 };
 
 app.initialize();
