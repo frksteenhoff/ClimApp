dc944b9dfe49fc315b17544d0bde868a3d688afc
Boris Kingma <boris.kingma@gmail.com>
2019-03-20 15:45:13 +0100

    .gitignore fix

--- cordova/climapp/node_modules/cordova-wheel-selector-plugin/examples/testapp/link-remote.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#get the released plugin in npm registry
-cordova plugin rm cordova-wheel-selector-plugin
-cordova plugin add cordova-wheel-selector-plugin
--- cordova/climapp/platforms/ios/ClimApp/Plugins/cordova-wheel-selector-plugin/SelectorCordovaPlugin.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Cordova/CDVPlugin.h>
-
-@interface SelectorCordovaPlugin : CDVPlugin
-
-- (void)showSelector:(CDVInvokedUrlCommand *)command;
-- (void)hideSelector:(CDVInvokedUrlCommand *)command;
-
-@end
