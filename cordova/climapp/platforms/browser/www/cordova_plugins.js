b7eba9ea562945944f64721c1c7f160555813fd3
Henriette Steenhoff <henriette.steenhoff@gmail.com>
2019-03-27 10:11:19 +0100

    removed unused plugins before merge

--- cordova/climapp/platforms/browser/www/cordova_plugins.js
+++ cordova/climapp/platforms/browser/www/cordova_plugins.js
@@ -35,36 +35,6 @@ module.exports = [
         "clobbers": [
             "window.plugins.toast"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-badge/www/badge.js",
-        "id": "cordova-plugin-badge.Badge",
-        "pluginId": "cordova-plugin-badge",
-        "clobbers": [
-            "cordova.plugins.notification.badge"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-badge/src/browser/favico.min.js",
-        "id": "cordova-plugin-badge.Badge.Favico",
-        "pluginId": "cordova-plugin-badge",
-        "clobbers": [
-            "cordova.plugins.notification.badge.Favico"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-badge/src/browser/BadgeProxy.js",
-        "id": "cordova-plugin-badge.Badge.Proxy",
-        "pluginId": "cordova-plugin-badge",
-        "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-local-notification/www/local-notification.js",
-        "id": "cordova-plugin-local-notification.LocalNotification",
-        "pluginId": "cordova-plugin-local-notification",
-        "clobbers": [
-            "cordova.plugins.notification.local"
-        ]
     }
 ];
 module.exports.metadata = 
@@ -75,10 +45,7 @@ module.exports.metadata =
     "cordova-plugin-splashscreen": "5.0.2",
     "cordova-plugin-whitelist": "1.3.3",
     "cordova-wheel-selector-plugin": "1.0.0",
-    "cordova-plugin-x-toast": "2.7.2",
-    "cordova-plugin-browsersync": "0.1.7",
-    "cordova-plugin-badge": "0.8.8",
-    "cordova-plugin-local-notification": "0.9.0-beta.3"
+    "cordova-plugin-x-toast": "2.7.2"
 }
 // BOTTOM OF METADATA
 });
