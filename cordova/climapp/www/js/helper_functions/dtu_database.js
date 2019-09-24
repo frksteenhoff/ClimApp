e30d455ae865254249d7833b0a096ce94b4571a1
Henriette Steenhoff <henriette.steenhoff@gmail.com>
2019-09-24 09:57:41 +0200

    Added API call for retrieving predicted indoor temp

--- cordova/climapp/www/js/helper_functions/dtu_database.js
+++ cordova/climapp/www/js/helper_functions/dtu_database.js
@@ -132,3 +132,33 @@ function getAppIDFromDB(kb) {
         });
     })
 }
+
+function getIndoorPrediction(kb) {
+    return new Promise((resolve, reject) => {
+        let apicall = "getIndoorPrediction";
+        let url = kb.server.dtu_ip + kb.server.dtu_api_base_url + apicall;
+        let user_data = {
+            "wo": kb.user.settings.open_windows, // window opening 0/1
+            "trv": kb.user.settings.thermostat_level, // heating setpoint (radiator valve/thermostat)
+            "cy": 1950, // building construction year [1920,1930 .. 2010]
+            "fa": 40, // floor area 5-200m^2 
+            "no": 3, // number of occupants in room (1-5)
+            "tao": kb.thermalindices.ireq[0].Tair, // outdoor temp (degrees celcius)
+            "rho": kb.thermalindices.ireq[0].rh, // outdoor relative humidity %
+            "sr": kb.thermalindices.ireq[index].rad, // solar radiation W/m^2
+        }
+        
+        //@Henriette, .done is not always reached
+        $.get(url, user_data).done(function (data, status, xhr) {
+            if (status === "success") {
+                let response = JSON.parse(data);
+                let indoorTemperature = response.temp;
+                console.log("Retrieved value from server: " + indoorTemperature);
+                resolve(indoorTemperature);
+            } else {
+                console.log("Failed to retrieve temperature from server.");
+                reject(false);
+            }
+        })
+    })
+}
